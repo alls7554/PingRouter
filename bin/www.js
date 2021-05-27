52781160429558ccb986660950ec64dc78afc063
alls7554 <alls970459@gmail.com>
2021-05-27 17:58:35 +0900

    modify PORT

--- bin/www.js
+++ bin/www.js
@@ -3,7 +3,7 @@
 const app = require('../app');
 const server = require('http').createServer(app);
 const io = require('../src/services/socket.io')(server);
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5500;
 
 server.listen(PORT, () => {
   console.log(`Express server has started on port ${PORT}`);
